6557371fa90f8e3d1523c026722794ad9ea150ff
birdgg <birdeggegg@gmail.com>
2023-06-09 19:36:51 +0800

    fix: add back revision

--- test/contracts/revision.test.ts
+++ test/contracts/revision.test.ts
@@ -5,16 +5,18 @@ import { mockUser } from '../mock'
 const contract = new Contract(mockUser.privateKey)
 
 describe('revision', () => {
-  // it.concurrent('should return the latest revision', async ({ expect }) => {
-  //   const { data: latest } = await contract.revision.getLatest()
-  //   expect(latest > 0n).toBe(true)
-  //   const { data: current } = contract.revision.getCurrent()
-  //   expect(current).toBe(latest)
-  // })
-  // it.concurrent('check', async ({ expect }) => {
-  //   const res = await contract.revision.check()
-  //   expect(res.data.currentRevision > 0n).toBe(true)
-  //   expect(res.data.isUpToDate).is.a('boolean')
-  //   expect(res.data.latestRevision > 0n).toBe(true)
-  // })
+  it.concurrent('should return the latest revision', async ({ expect }) => {
+    const { data: latest } = await contract.revision.getLatest()
+    // eslint-disable-next-line no-console
+    console.log(latest)
+    expect(latest > 0n).toBe(true)
+    const { data: current } = contract.revision.getCurrent()
+    expect(current).toBe(latest)
+  })
+  it.concurrent('check', async ({ expect }) => {
+    const res = await contract.revision.check()
+    expect(res.data.currentRevision > 0n).toBe(true)
+    expect(res.data.isUpToDate).is.a('boolean')
+    expect(res.data.latestRevision > 0n).toBe(true)
+  })
 })
