915c900e52a4845afe992a4afd6c4630e1d35de2
birdgg <birdeggegg@gmail.com>
2023-06-09 23:01:55 +0800

    fix: add headers

--- test/indexer/newbie.test.ts
+++ test/indexer/newbie.test.ts
@@ -1,17 +1,17 @@
-import { describe, expect, test } from 'vitest'
+import { describe, it } from 'vitest'
 import { createIndexer } from '../../src'
 import { mockUser } from '../mock'
 
 const indexer = createIndexer()
 
 describe('newbie', () => {
-  test('sign in', async () => {
+  it('sign in', async ({ expect }) => {
     const token = await indexer.newbie.signIn(mockUser.email, mockUser.password)
     expect(token).toBeTruthy()
     expect(token).toEqual(indexer.newbie.token)
   })
 
-  test('current user', async () => {
+  it('current user', async ({ expect }) => {
     const account = await indexer.newbie.getAccount()
     expect(account.email).toEqual(mockUser.email)
     expect(account.characterId).to.be.a('number')
