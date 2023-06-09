08f2129559d2eea2642e216cbe74f9727bb7ba8f
birdgg <birdeggegg@gmail.com>
2023-06-09 22:17:04 +0800

    fix: use promise

--- test/indexer/notification.test.ts
+++ test/indexer/notification.test.ts
@@ -5,14 +5,16 @@ const indexer = createIndexer()
 const characterId = 55926
 
 describe('notification', () => {
-  test('get many', async () => {
-    const notifications = await indexer.notification.getMany(characterId)
-    expect(notifications.list).is.an('array')
-    expect(notifications.count).is.an('number')
+  test('get many', () => {
+    indexer.notification.getMany(characterId).then((notifications) => {
+      expect(notifications.list).is.an('array')
+      expect(notifications.count).is.an('number')
+    })
   })
 
-  test('unread count', async () => {
-    const { count } = await indexer.notification.getUnreadCount(characterId)
-    expect(count).is.a('number')
+  test('unread count', () => {
+    indexer.notification.getUnreadCount(characterId).then(({ count }) => {
+      expect(count).is.a('number')
+    })
   })
 })
