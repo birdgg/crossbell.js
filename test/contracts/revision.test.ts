d594d81f44c7b46f9e3892ad3d62b3af2d981320
birdgg <birdeggegg@gmail.com>
2023-06-09 20:17:58 +0800

    fix: remove concurrency

--- test/contracts/revision.test.ts
+++ test/contracts/revision.test.ts
@@ -1,19 +1,18 @@
-import { describe, it } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { Contract } from '../../src'
 import { mockUser } from '../mock'
 
 const contract = new Contract(mockUser.privateKey)
 
 describe('revision', () => {
-  it.concurrent('should return the latest revision', async ({ expect }) => {
+  test('should return the latest revision', async () => {
     const { data: latest } = await contract.revision.getLatest()
     // eslint-disable-next-line no-console
-    console.log(latest)
     expect(latest > 0n).toBe(true)
     const { data: current } = contract.revision.getCurrent()
     expect(current).toBe(latest)
   })
-  it.concurrent('check', async ({ expect }) => {
+  test('check', async () => {
     const res = await contract.revision.check()
     expect(res.data.currentRevision > 0n).toBe(true)
     expect(res.data.isUpToDate).is.a('boolean')
