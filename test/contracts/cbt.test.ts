c76394d2efcdddd58b548fce31f372a442e37968
birdgg <birdeggegg@gmail.com>
2023-06-09 21:49:04 +0800

    fix: remove contract tests

--- test/contracts/cbt.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe } from 'vitest'
-// import { Contract } from '../../src'
-// import { mockUser } from '../mock'
-
-// const contract = new Contract(mockUser.privateKey)
-
-describe.todo('cbt', () => {
-  //
-})
