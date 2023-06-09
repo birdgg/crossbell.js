1e7c3878a93c2206305f40213e9ce27ff0e4c52e
birdgg <birdeggegg@gmail.com>
2023-06-09 23:07:52 +0800

    fix: ci

--- test/indexer/newbie.test.ts
+++ test/indexer/newbie.test.ts
@@ -1,17 +1,17 @@
-import { describe, expect, it } from 'vitest'
+import { describe, it } from 'vitest'
 import { createIndexer } from '../../src'
 import { mockUser } from '../mock'
 
 const indexer = createIndexer()
 
 describe('newbie', () => {
-  it('sign in', async () => {
+  it('sign in', async ({ expect }) => {
     const token = await indexer.newbie.signIn(mockUser.email, mockUser.password)
     expect(token).toBeTruthy()
     expect(token).toEqual(indexer.newbie.token)
   })
 
-  it('current user', async () => {
+  it('current user', async ({ expect }) => {
     const account = await indexer.newbie.getAccount()
     expect(account.email).toEqual(mockUser.email)
     expect(account.characterId).to.be.a('number')
